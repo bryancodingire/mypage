c3259b94ae51ff5c07537d2c0c330ed912b247e4
Bryan <laurenkelley@Laurens-MacBook-Pro-69.local>
2020-09-01 21:12:03 +0100

    Updates

---
+++ precache-manifest.b51d1cd4d8917df8484e0d4e3688f207.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc25430e24df4f2b3669fa2b171440f9",
+    "url": "/mypage/index.html"
+  },
+  {
+    "revision": "4cbbd4cd60d306e8ff78",
+    "url": "/mypage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "efa4ab12e03387b74655",
+    "url": "/mypage/static/js/2.8202b758.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mypage/static/js/2.8202b758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbbd4cd60d306e8ff78",
+    "url": "/mypage/static/js/main.74dbe0de.chunk.js"
+  },
+  {
+    "revision": "f62fe814d257ca801b20",
+    "url": "/mypage/static/js/runtime-main.cc316ea1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mypage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bb67004a231f75b00ce476437a854165",
+    "url": "/mypage/static/media/thisismydesign.bb67004a.gif"
+  }
+]);
